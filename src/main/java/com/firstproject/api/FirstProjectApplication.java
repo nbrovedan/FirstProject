90c606a98fbf19685d0b764182e89a2f7c4af796
Naidion Brovedan <naidion@MacBook-Pro-de-Naidion.local>
2018-06-09 10:52:17 -0300

    first commit

---
+++ src/main/java/com/firstproject/api/FirstProjectApplication.java
@@ -0,0 +1,12 @@
+package com.firstproject.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectApplication.class, args);
+	}
+}
